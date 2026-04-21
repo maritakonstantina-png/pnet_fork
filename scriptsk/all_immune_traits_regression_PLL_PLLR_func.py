2055753bf8c63c32f92f0dffa5a3bcc2e4aeaeb0
Konstantina Marita <maritakonstantina@gmail.com>
2026-04-21 12:22:57 +0200

    kept only one immune trait for the first run of pnet

--- scriptsk/all_immune_traits_regression_PLL_PLLR_func.py
+++ scriptsk/all_immune_traits_regression_PLL_PLLR_func.py
@@ -2,24 +2,24 @@ import os
 import time
 
 immune_traits = [
-  "NK cells",
-  "T helper cells" ,
-  "CD8 T cells" ,
-  "Tfh cells",
-  "AMP1" ,
-  "Term cells"
-  "aDC" ,
-  "Tcm cells" ,
-  "Eosinophils",
-  "Interferon Cluster 21214954" ,
-  "GP11 Immune IFN" ,
-  "Interferon 19272155" ,
-  "Th1 cells" ,
-  "Neutrophils"  ,
-  "Th17 cells" 
-  ]
+  "NK cells"]
+ # "T helper cells" ,
+  #"CD8 T cells" ,
+ # "Tfh cells",
+  #"AMP1" ,
+ # "Term cells"
+ # "aDC" ,
+ # "Tcm cells" ,
+  #"Eosinophils",
+  #"Interferon Cluster 21214954" ,
+  #"GP11 Immune IFN" ,
+ # "Interferon 19272155" ,
+ # "Th1 cells" ,
+ # "Neutrophils"  ,
+ # "Th17 cells" 
+ # ]
 
-score_types = ["PLL","PLLR"]
+score_types = ["PLL"] #,"PLLR"]
 
 for trait in immune_traits: 
     for score in score_types:
@@ -31,5 +31,5 @@ for trait in immune_traits:
                     -o "+LOGDIR+" \
                     -e "+LOGDIR+" \
                     -v score_type=\""+score+"\",immune_traits=\""+trait+"\" \
-                    /shares/CIBIO-Storage/BCG/scratch/kmarita/code/pnet_fork/scriptsk/pbs_script.sh"
+                    /shares/CIBIO-Storage/BCG/scratch/kmarita/code/pnet_fork/scriptsk/PBS_scripts/all_cancers_prediction_bc_all_scores.sh"
         os.system(command)
