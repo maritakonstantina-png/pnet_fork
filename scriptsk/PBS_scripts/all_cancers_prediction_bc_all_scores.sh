76facd31a041893f3594d48c3210b356c650e8a2
Konstantina Marita <maritakonstantina@gmail.com>
2026-04-21 13:26:48 +0200

    Remove output redirection from regression script execution in PBS script

--- scriptsk/PBS_scripts/all_cancers_prediction_bc_all_scores.sh
+++ scriptsk/PBS_scripts/all_cancers_prediction_bc_all_scores.sh
@@ -37,7 +37,7 @@ echo "Assigned to GPU: $CUDA_VISIBLE_DEVICES"
 start=$(date)
 
 # SCRIPT
-python /shares/CIBIO-Storage/BCG/scratch/kmarita/code/pnet_fork/scriptsk/regression_immune_traits.py" >> "/shares/CIBIO-Storage/BCG/scratch/kmarita/code/pnet_fork/scriptsk/output_pnet/regression_immune_traits_$PBS_JOBID.log" 2>&1
+python /shares/CIBIO-Storage/BCG/scratch/kmarita/code/pnet_fork/scriptsk/regression_immune_traits.py"
 
 # NOTIFY END OF SCRIPT
-curl -d "Done $PBS_JOBID ($immune_traits) on cibioGPUQ launched at $start, finished at $(date)" ntfy.sh/kmarita
+curl -d "Done $PBS_JOBID $(immune_traits) on cibioGPUQ launched at $start, finished at $(date)" ntfy.sh/kmarita
